6d476ff741445db0baee5cdc830a22a6aa60fb27
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2017-12-22 14:56:15 +0800

    try to figure out HTTPS login and login interceptor

---
+++ src/main/java/com/xmlan/machine/common/interceptor/LoginInterceptor.java
@@ -0,0 +1,31 @@
+package com.xmlan.machine.common.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by ayakurayuki on 2017/12/22-14:42.
+ * Package: com.xmlan.machine.common.interceptor
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+
+}
