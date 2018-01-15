f7d9274e2328b857c354462b1e110624614d98e2
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2018-01-15 10:23:11 +0800

    充电状态更新接口

--- src/main/java/com/xmlan/machine/service/provider/AdvertisementMachineServiceProvider.groovy
+++ src/main/java/com/xmlan/machine/service/provider/AdvertisementMachineServiceProvider.groovy
@@ -1,9 +1,15 @@
 package com.xmlan.machine.service.provider
 
+import cn.jiguang.common.ClientConfig
+import cn.jiguang.common.resp.APIRequestException
+import cn.jpush.api.JPushClient
 import com.google.common.collect.Maps
 import com.xmlan.machine.common.base.BaseController
 import com.xmlan.machine.common.cache.AdvertisementMachineCache
+import com.xmlan.machine.common.config.Global
 import com.xmlan.machine.common.util.DateUtils
+import com.xmlan.machine.common.util.JsonUtils
+import com.xmlan.machine.common.util.PushUtils
 import com.xmlan.machine.common.util.StringUtils
 import com.xmlan.machine.module.advertisementMachine.entity.AdvertisementMachine
 import com.xmlan.machine.module.advertisementMachine.service.AdvertisementMachineService
@@ -43,5 +49,33 @@ class AdvertisementMachineServiceProvider extends BaseController {
         return map
     }
 
+    @RequestMapping(value = '/charge', produces = "application/json; charset=utf-8")
+    @ResponseBody
+    Map charge(int id, int operate) {
+        def responseCode = service.chargeControl(id, operate)
+        def map = Maps.newHashMap()
+        map['responseCode'] = responseCode
+        if (responseCode == NO_SUCH_ROW) {
+            map['message'] = "目标广告机不存在"
+        } else if (responseCode == ERROR_REQUEST) {
+            map['message'] = "操作码不正确"
+        } else if (responseCode == DONE) {
+            map['message'] = operate == 1 ? "开灯！" : "关灯！"
+            def command = Maps.newHashMap()
+            command['id'] = id
+            command['operate'] = operate
+            def pushClient = new JPushClient(Global.masterSecret, Global.appKey, null, ClientConfig.instance)
+            def payload = PushUtils.buildPayload("${id}", "Light switch.", JsonUtils.toJsonString(command))
+            try {
+                def result = pushClient.sendPush(payload)
+                logger.trace(result)
+            } catch (APIRequestException e) {
+                logger.error "API exception with: ${e.message}"
+            }
+        } else {
+            map['message'] = "系统繁忙"
+        }
+        return map
+    }
 
 }
