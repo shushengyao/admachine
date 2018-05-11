4d7ff948acb5902b240626102f9c2fc58defe24c
yss <beishan.yao@qq.com>
2018-05-11 11:23:16 +0800

    添加设备BUG修复；广告机内新增大气采集页面；项目新增视图功能

--- src/main/java/com/xmlan/machine/module/advertisementMachine/entity/MachineSensor.groovy
+++ src/main/java/com/xmlan/machine/module/advertisementMachine/entity/MachineSensor.groovy
@@ -14,4 +14,52 @@ class MachineSensor implements Serializable {
     String brightness
     String machineID
 
+    int getId() {
+        return id
+    }
+
+    String getTemperature() {
+        return temperature
+    }
+
+    String getHumidity() {
+        return humidity
+    }
+
+    String getPm25() {
+        return pm25
+    }
+
+    void setId(int id) {
+        this.id = id
+    }
+
+    void setTemperature(String temperature) {
+        this.temperature = temperature
+    }
+
+    void setHumidity(String humidity) {
+        this.humidity = humidity
+    }
+
+    void setPm25(String pm25) {
+        this.pm25 = pm25
+    }
+
+    void setBrightness(String brightness) {
+        this.brightness = brightness
+    }
+
+    void setMachineID(String machineID) {
+        this.machineID = machineID
+    }
+
+    String getBrightness() {
+
+        return brightness
+    }
+
+    String getMachineID() {
+        return machineID
+    }
 }
